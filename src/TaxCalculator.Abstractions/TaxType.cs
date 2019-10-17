e0cd82302ee47f81e6be6e4a25060dc2eb3f9392
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2019-10-17 07:12:58 +0200

    add federal tax data

---
+++ src/TaxCalculator.Abstractions/TaxType.cs
@@ -0,0 +1,10 @@
+﻿namespace PensionCoach.Tools.TaxCalculator.Abstractions
+{
+    public enum TaxType
+    {
+        Undefined = 0,
+        Income = 1,
+        Capital = 2,
+        Person = 3
+    }
+}
