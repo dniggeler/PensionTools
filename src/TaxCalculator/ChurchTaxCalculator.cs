aa21525f573644a03b5cafadf20493f90976e0b1
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2019-11-30 20:43:13 +0100

    refactor church tax calculator validation

--- src/TaxCalculator/ChurchTaxCalculator.cs
+++ src/TaxCalculator/ChurchTaxCalculator.cs
@@ -1,16 +1,15 @@
-﻿namespace TaxCalculator
-{
-    using System;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using FluentValidation;
-    using LanguageExt;
-    using PensionCoach.Tools.TaxCalculator.Abstractions;
-    using PensionCoach.Tools.TaxCalculator.Abstractions.Models;
-    using PensionCoach.Tools.TaxCalculator.Abstractions.Models.Person;
-    using Tax.Data;
-    using Tax.Data.Abstractions.Models;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using LanguageExt;
+using PensionCoach.Tools.TaxCalculator.Abstractions;
+using PensionCoach.Tools.TaxCalculator.Abstractions.Models;
+using PensionCoach.Tools.TaxCalculator.Abstractions.Models.Person;
+using Tax.Data;
+using Tax.Data.Abstractions.Models;
 
+namespace TaxCalculator
+{
     public class ChurchTaxCalculator : IChurchTaxCalculator
     {
         private readonly IValidator<ChurchTaxPerson> churchTaxPersonValidator;
@@ -31,18 +30,22 @@
         public Task<Either<string, ChurchTaxResult>> CalculateAsync(
             int calculationYear, ChurchTaxPerson person, AggregatedBasisTaxResult taxResult)
         {
+            return this
+                .Validate(person, taxResult)
+                .BindAsync(r => this.CalculateInternalAsync(calculationYear, person, taxResult));
+        }
+
+        private Either<string, bool> Validate(
+            ChurchTaxPerson person, AggregatedBasisTaxResult taxResult)
+        {
             if (person == null)
             {
-                return Task
-                    .FromResult<Either<string, ChurchTaxResult>>(
-                        $"validation failed: {nameof(person)} null");
+                return $"validation failed: {nameof(person)}";
             }
 
             if (taxResult == null)
             {
-                return Task
-                    .FromResult<Either<string, ChurchTaxResult>>(
-                        $"validation failed: {nameof(taxResult)} null");
+                return $"validation failed: {nameof(taxResult)} null";
             }
 
             var validationResult = this.churchTaxPersonValidator.Validate(person);
@@ -51,9 +54,8 @@
             {
                 var errorMessageLine = string.Join(
                     ";", validationResult.Errors.Select(x => x.ErrorMessage));
-                return Task
-                    .FromResult<Either<string, ChurchTaxResult>>(
-                        $"validation failed: {errorMessageLine}");
+
+                return $"validation failed: {errorMessageLine}";
             }
 
             var validationResultForTax = this.taxResultValidator.Validate(taxResult);
@@ -61,20 +63,19 @@
             if (!validationResultForTax.IsValid)
             {
                 var errorMessageLine = string.Join(
-                    ";", validationResult.Errors.Select(x => x.ErrorMessage));
-                return Task
-                    .FromResult<Either<string, ChurchTaxResult>>(
-                        $"validation failed: {errorMessageLine}");
+                    ";", validationResultForTax.Errors.Select(x => x.ErrorMessage));
+
+                return $"validation failed: {errorMessageLine}";
             }
 
-            return this.CalculateInternalAsync(calculationYear, person, taxResult);
+            return true;
         }
 
         private Task<Either<string, ChurchTaxResult>> CalculateInternalAsync(
             int calculationYear, ChurchTaxPerson person, AggregatedBasisTaxResult taxResult)
         {
-            var religiousGroupPartner = Prelude.Optional(
-                person.PartnerReligiousGroup.IfNone(ReligiousGroupType.Other));
+            Option<ReligiousGroupType> religiousGroupPartner =
+                person.PartnerReligiousGroup.IfNone(ReligiousGroupType.Other);
 
             var splitFactor =
                 (from c in person.CivilStatus
@@ -84,32 +85,32 @@
                      c, r, rp))
                 .IfNone(0M);
 
-            TaxRateModel taxRateModel = this.taxRateContext.Rates
-                .Single(item => item.Year == calculationYear
+            Option<TaxRateModel> taxRateModel = this.taxRateContext.Rates
+                .FirstOrDefault(item => item.Year == calculationYear
                                 && item.Canton == person.Canton
                                 && item.Municipality == person.Municipality);
 
-            Option<ChurchTaxResult> result =
-                from ratePerson in person.ReligiousGroup.Map(GetTaxRate)
-                from ratePartner in religiousGroupPartner.Map(GetTaxRate)
-                select new ChurchTaxResult
-                {
-                    TaxAmount = ratePerson / 100M * taxResult.Total * splitFactor,
-                    TaxAmountPartner = ratePartner / 100M * taxResult.Total * (1M - splitFactor),
-                };
-
-            return result.Match<Either<string, ChurchTaxResult>>(
+            return
+                (from m in taxRateModel
+                    from ratePerson in person.ReligiousGroup.Map(type => GetTaxRate(type, m))
+                    from ratePartner in religiousGroupPartner.Map(type => GetTaxRate(type, m))
+                    select new ChurchTaxResult
+                    {
+                        TaxAmount = ratePerson / 100M * taxResult.Total * splitFactor,
+                        TaxAmountPartner = ratePartner / 100M * taxResult.Total * (1M - splitFactor),
+                    })
+                .Match<Either<string, ChurchTaxResult>>(
                     Some: r => r,
-                    None: () => "Calculation failed")
+                    None: () => "No rate for church tax found")
                 .AsTask();
 
-            decimal GetTaxRate(ReligiousGroupType religiousGroupType)
+            decimal GetTaxRate(ReligiousGroupType religiousGroupType, TaxRateModel rateModel)
             {
                 return religiousGroupType switch
                 {
-                    ReligiousGroupType.Roman => taxRateModel.RomanChurchTaxRate,
-                    ReligiousGroupType.Protestant => taxRateModel.ProtestantChurchTaxRate,
-                    ReligiousGroupType.Catholic => taxRateModel.CatholicChurchTaxRate,
+                    ReligiousGroupType.Roman => rateModel.RomanChurchTaxRate,
+                    ReligiousGroupType.Protestant => rateModel.ProtestantChurchTaxRate,
+                    ReligiousGroupType.Catholic => rateModel.CatholicChurchTaxRate,
                     _ => 0M
                 };
             }
