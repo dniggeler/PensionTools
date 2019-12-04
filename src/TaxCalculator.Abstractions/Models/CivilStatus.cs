222b53634e199dd06fce56a2ea198f06122e1617
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2019-12-04 07:03:22 +0100

    convert enum type to string

--- src/TaxCalculator.Abstractions/Models/CivilStatus.cs
+++ src/TaxCalculator.Abstractions/Models/CivilStatus.cs
@@ -1,9 +1,24 @@
-﻿namespace PensionCoach.Tools.TaxCalculator.Abstractions.Models
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace PensionCoach.Tools.TaxCalculator.Abstractions.Models
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum CivilStatus
     {
+        /// <summary>
+        /// Undefined is not a valid and is treated as an error
+        /// </summary>
         Undefined = 0,
-        Single = 1, 
+
+        /// <summary>
+        /// Single means not married, ie. widowed and divorced persons are treated as singles
+        /// </summary>
+        Single = 1,
+
+        /// <summary>
+        /// A married person
+        /// </summary>
         Married = 2
     }
 }
--- src/TaxCalculator.Abstractions/Models/ReligiousGroupType.cs
+++ src/TaxCalculator.Abstractions/Models/ReligiousGroupType.cs
@@ -1,10 +1,31 @@
-﻿namespace PensionCoach.Tools.TaxCalculator.Abstractions.Models
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+
+namespace PensionCoach.Tools.TaxCalculator.Abstractions.Models
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum ReligiousGroupType
     {
-        Other, 
-        Protestant,
-        Catholic,
-        Roman
+        /// <summary>
+        /// Undefined or none of the following
+        /// </summary>
+        Other=0,
+
+        /// <summary>
+        /// reformiert in german
+        /// </summary>
+        Protestant =1,
+
+        /// <summary>
+        /// katholisch in german
+        /// </summary>
+        Catholic =2,
+
+        /// <summary>
+        /// römisch-katholisch in german
+        /// </summary>
+        Roman =3
     }
 }
