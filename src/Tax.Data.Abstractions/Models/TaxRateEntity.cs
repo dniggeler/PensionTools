e17233bbb851e63f13ce7d4af5ab5cd20fa1dd5b
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2020-01-03 18:34:11 +0100

    change data

---
+++ src/Tax.Data.Abstractions/Models/TaxRateEntity.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Tax.Data.Abstractions.Models
+{
+    public class TaxRateEntity
+    {
+        [Column("BfsId")] 
+        public int BfsId { get; set; }
+
+        [Column("Jahr")]
+        public int Year { get; set; }
+
+        [Column("Kanton")]
+        public string Canton { get; set; }
+
+        [Column("Gemeindename")]
+        public string MunicipalityName { get; set; }
+        
+        [Column("SteuerfussKanton")]
+        public decimal TaxRateCanton { get; set; }
+
+        [Column("SteuerfussGemeinde")]
+        public decimal TaxRateMunicipality { get; set; }
+
+        [Column("KircheRoemischKatholisch")]
+        public decimal RomanChurchTaxRate { get; set; }
+
+        [Column("KircheReformiert")]
+        public decimal ProtestantChurchTaxRate { get; set; }
+
+        [Column("KircheChristKatholisch")]
+        public decimal CatholicChurchTaxRate { get; set; }
+    }
+}
