d405ae4de099a2275ad5f0f0b2d0db161bb0e2be
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2020-02-09 01:12:04 +0700

    implement skeleton tax comparer

---
+++ src/Tax.Tools.Comparison/Tax.Tools.Comparison.Abstractions/ITaxComparer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using LanguageExt;
+using PensionCoach.Tools.TaxCalculator.Abstractions.Models;
+using PensionCoach.Tools.TaxCalculator.Abstractions.Models.Person;
+
+namespace Tax.Tools.Comparison.Abstractions
+{
+    public interface ITaxComparer
+    {
+        Either<string,IReadOnlyCollection<FullCapitalBenefitTaxResult>> CompareCapitalBenefitTaxAsync(
+            int calculationYear, int municipalityId, Canton canton, CapitalBenefitTaxPerson person);
+    }
+}
---
+++ src/Tax.Tools.Comparison/Tax.Tools.Comparison/TaxComparer.cs
@@ -0,0 +1,16 @@
+﻿using LanguageExt;
+using PensionCoach.Tools.TaxCalculator.Abstractions.Models;
+using PensionCoach.Tools.TaxCalculator.Abstractions.Models.Person;
+using System.Collections.Generic;
+using Tax.Tools.Comparison.Abstractions;
+
+namespace Tax.Tools.Comparison
+{
+    public class TaxComparer : ITaxComparer
+    {
+        public Either<string, IReadOnlyCollection<FullCapitalBenefitTaxResult>> CompareCapitalBenefitTaxAsync(int calculationYear, int municipalityId, Canton canton, CapitalBenefitTaxPerson person)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
