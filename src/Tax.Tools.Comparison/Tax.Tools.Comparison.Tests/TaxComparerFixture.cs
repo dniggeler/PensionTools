644866760abc9086e09aa8bea18d5a820522c492
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2020-02-10 00:25:22 +0700

    work in progress

---
+++ src/Tax.Tools.Comparison/Tax.Tools.Comparison.Tests/TaxComparerFixture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Tax.Tools.Comparison.Tests
+{
+    public class TaxComparerFixture<T>
+        where T : class
+    {
+        public ServiceProvider Provider { get; }
+
+        public T Calculator { get; }
+
+        public T Service { get; }
+
+        public TaxComparerFixture()
+        {
+            var projectPath = Assembly.GetExecutingAssembly()
+                .Location.Split("src", StringSplitOptions.RemoveEmptyEntries)
+                .First();
+
+            var dbFile = Path.Combine(projectPath, @"src\Tax.Data\files\TaxDb.db");
+
+            var configurationDict = new Dictionary<string, string>
+            {
+                {"ConnectionStrings:TaxDb", dbFile}
+            };
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configurationDict)
+                .Build();
+
+            ServiceCollection coll = new ServiceCollection();
+
+            coll.AddLogging();
+            coll.AddTaxCalculators();
+
+            Provider = coll.BuildServiceProvider();
+
+            Calculator = Provider.GetRequiredService<T>();
+
+            Service = Provider.GetRequiredService<T>();
+        }
+    }
+}
---
+++ src/Tax.Tools.Comparison/Tax.Tools.Comparison/TaxComparerServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Tax.Tools.Comparison.Abstractions;
+
+
+namespace Tax.Tools.Comparison
+{
+    public static class TaxComparerServiceCollectionExtensions
+    {
+        public static void AddTaxCalculators(this IServiceCollection collection)
+        {
+            collection.AddTransient<ITaxComparer, TaxComparer>();
+        }
+    }
+}
