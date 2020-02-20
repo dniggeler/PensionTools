8f700df4e21ce88ae8239f4d1904c02179bbb42f
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2020-02-20 21:30:05 +0100

    SAVEPOINT

--- src/PensionTools.App.Vue/Vue/pensiontools/src/App.vue
+++ src/PensionTools.App.Vue/Vue/pensiontools/src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <div id="app">
     <header>
-      <span>Vue.js PWA</span>
+      <b-navbar toggleable="md" type="light" variant="light">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-brand to="/">Swiss Tax and Pension Tools</b-navbar-brand>
+        <b-collapse is-nav id="nav-collapse">
+          <b-navbar-nav>
+            <b-nav-item href="#" @click.prevent="login" v-if="!user">Login</b-nav-item>
+            <b-nav-item href="#" @click.prevent="logout" v-else>Logout</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
       <router-view></router-view>
     </main>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        user: null
+      }
+    },
+    methods: {
+      login () {
+
+      },
+      async logout () {
+
+      }
+    }
+  }
 </script>
 
 <style>
@@ -37,7 +59,7 @@ header {
   margin: 0;
   height: 56px;
   padding: 0 16px 0 24px;
-  background-color: #35495E;
+  background-color: #f8f9fa;
   color: #ffffff;
 }
 
--- src/PensionTools.App.Vue/Vue/pensiontools/src/components/Hello.vue
+++ src/PensionTools.App.Vue/Vue/pensiontools/src/components/Hello.vue
@@ -1,31 +1,16 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="http://chat.vuejs.org/" target="_blank" rel="noopener">Vue Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank" rel="noopener">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+	<h1>{{ title }}</h1>
+	<h2>Collection of litte tools to analyze your financial situation</h2>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js PWA'
-    }
+	  return {
+  	  title: 'Swiss tax and pension tools'
+	  }
   }
 }
 </script>
