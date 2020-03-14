a8dde2f632b22682aa5c9d4e1d5cb2d7b476ab99
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2020-03-14 12:11:34 +0100

    add expandble drawer

--- src/vue.app.ui/src/App.vue
+++ src/vue.app.ui/src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <v-app>
     <v-app-bar app>
-
       <v-toolbar>
-        <v-app-bar-nav-icon @click="drawer=!drawer"/>
+        <v-app-bar-nav-icon @click="drawer=!drawer" />
         <v-toolbar-title>Swiss Tax and Pension Tools</v-toolbar-title>
 
         <v-toolbar-items>
@@ -13,64 +12,61 @@
           </v-btn>
           <v-menu offset-y>
             <template v-slot:activator="{ on }">
-              <v-btn slot="activator" v-on="on">
-              <v-icon left>mdi-hammer-screwdriver</v-icon>
-              <span>Tools</span>
+              <v-btn slot="activator" v-on="on" to="/tools">
+                <v-icon left>mdi-hammer-screwdriver</v-icon>
+                <span>Tools</span>
               </v-btn>
             </template>
             <v-list>
-              <v-list-item v-for="link in links[1].sublinks" :key="link.key" @click="callback">
-                <v-list-item-title>{{ link.key }}</v-list-item-title>
+              <v-list-item v-for="link in links[1].sublinks" :key="link.key">
+                <v-list-item-title>{{ link.text }}</v-list-item-title>
               </v-list-item>
             </v-list>
           </v-menu>
-          <v-btn text to="/tools">
-            <v-icon left>mdi-hammer-screwdriver</v-icon>
-            <span>Tools</span>
-          </v-btn>
+
           <v-btn text to="/municipalities">
             <v-icon left>mdi-database</v-icon>
             <span>Data</span>
           </v-btn>
         </v-toolbar-items>
 
-        <v-spacer/>
+        <v-spacer />
 
-        <v-btn icon
-            right
-            href="https://github.com/dniggeler/PensionTools" 
-            target="_blank"
-          >
+        <v-btn icon right href="https://github.com/dniggeler/PensionTools" target="_blank">
           <v-icon>mdi-github-circle</v-icon>
-          </v-btn>
+        </v-btn>
       </v-toolbar>
-
     </v-app-bar>
-    <v-navigation-drawer 
-      v-model="drawer"
-      app
-    >
-      <v-list>
-        <v-subheader class="title">Application</v-subheader>
-        <v-list-item v-for="(link,i) in links" :key="i" router :to="link.route">
-          <v-list-item-action>
-            <v-icon>
-              {{ link.icon }}
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>
-              {{ link.text }}
-            </v-list-item-title>
-          </v-list-item-content>
-          <v-list-item-action>
-            <v-icon>mdi-menu-down</v-icon>
-          </v-list-item-action>
-        </v-list-item>
-      </v-list>
-
+    <v-navigation-drawer v-model="drawer" app>
+      <v-expansion-panels flat class="pr-1">
+        <v-expansion-panel
+          :multiple="multiple"
+          tile="true"
+          expand
+          focusable
+          :readonly="!link.sublinks"
+          v-for="(link,i) in links"
+          :key="i"
+          router
+          :to="link.route"
+        >
+          <v-expansion-panel-header>
+            <div>
+              <v-icon class="pr-1">{{ link.icon }}</v-icon>
+              <span>{{link.text}}</span>
+            </div>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-list>
+              <v-list-item v-for="(sublink,i) in link.sublinks" :key="i">
+                {{ sublink.text}}
+              </v-list-item>
+            </v-list>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-navigation-drawer>
-      
+
     <router-view />
     <v-footer></v-footer>
   </v-app>
@@ -82,12 +78,25 @@ export default {
 
   data: () => ({
     drawer: false,
+    multiple: true,
     links: [
-      { text: 'Home', icon: 'mdi-home', sublinks: [], route: '/' },
-      { text: 'Tools', icon: 'mdi-hammer-screwdriver', sublinks: [
-        { key: 'IncomeTax', text: 'Income Tax' },
-        { key: 'BenefitTax', text: 'Benefit Tax' }], route: '/tools' },
-      { text: 'Data', icon: 'mdi-database', sublinks: [], route: '/municipalities' }
+      { key: "home", text: "Home", icon: "mdi-home", route: "/" },
+      {
+        key: "tools",
+        text: "Tools",
+        icon: "mdi-hammer-screwdriver",
+        sublinks: [
+          { key: "IncomeTax", text: "Income Tax" },
+          { key: "BenefitTax", text: "Benefit Tax" }
+        ],
+        route: "/tools"
+      },
+      {
+        key: "tools",
+        text: "Data",
+        icon: "mdi-database",
+        route: "/municipalities"
+      }
     ]
   })
 };
