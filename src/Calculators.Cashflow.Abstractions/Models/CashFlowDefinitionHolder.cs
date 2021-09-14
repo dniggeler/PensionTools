5f0a50d40988a7168aa4e10c27274639e6b1e412
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2021-09-14 23:31:49 +0200

    WIP

---
+++ src/Calculators.Cashflow.Abstractions/Models/CashFlowDefinitionHolder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Calculators.CashFlow.Models
+{
+    public record CashFlowDefinitionHolder
+    {
+        /// <summary>
+        /// Gets the generic cash flow definitions.
+        /// </summary>
+        /// <value>
+        /// The generic cash flow definitions.
+        /// </value>
+        public IReadOnlyCollection<GenericCashFlowDefinition> GenericCashFlowDefinitions { get; init; }
+
+        /// <summary>
+        /// Gets the clear cash flow definitions.
+        /// </summary>
+        /// <value>
+        /// The clear cash flow definitions.
+        /// </value>
+        public IReadOnlyCollection<ClearCashFlowDefinition> ClearCashFlowDefinitions { get; init; }
+    }
+}
