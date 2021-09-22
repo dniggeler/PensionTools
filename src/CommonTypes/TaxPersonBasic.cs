78dd313e1f15654093705678e136af1f488fe057
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2021-09-22 06:48:22 +0200

    SAVEPOINT

---
+++ src/CommonTypes/TaxPersonBasic.cs
@@ -0,0 +1,11 @@
+﻿namespace PensionCoach.Tools.CommonTypes
+{
+    public record TaxPersonBasic
+    {
+        public string Name { get; init; }
+        public CivilStatus CivilStatus { get; init; }
+        public int NumberOfChildren { get; init; }
+        public ReligiousGroupType ReligiousGroupType { get; init; }
+        public ReligiousGroupType? PartnerReligiousGroupType { get; init; }
+    };
+}
