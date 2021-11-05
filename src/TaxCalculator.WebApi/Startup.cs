e2e1a11a5109be06d46dc23e83acd93538bbc755
dniggeler <dniggeler@bhfs.ch>
2021-11-05 15:47:29 +0100

    Nidi/support blazor app build (#34)
    * FIX UNIT TESTS
    
    * fix docker file
    
    * fix municipality not found message
    
    * add new action to simulate change of residence
    
    * use change residence action in calculation workflow
    
    * DO VARIOUS CLEANUP
    
    * WIP
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * WIP
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * DO VARIOUS RENAMING
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * fix Endlos-Schlaufe in Browser
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * add integration for municipality service
    
    * SAVEPOINT
    
    * wire real municipality service up
    
    * extend valid origins
    
    * SAVEPOINT
    
    * DO VARIOUS RENAMING
    
    * return all municipalities supporting all tax types
    
    * remove year from tax supporting municipality list
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * fix tye config and app settings for development
    
    * setup tye
    
    * support growth rate for wealth and capital benefits
    
    * add release profile for local testing with remote web api
    
    * add yaml for blazor app
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * SAVEPOINT
    
    * allow azure storage blob endpoint
    
    * SAVEPOINT
    
    Co-authored-by: Dieter Niggeler <dieter.niggeler@swisslife.ch>

--- src/TaxCalculator.WebApi/Startup.cs
+++ src/TaxCalculator.WebApi/Startup.cs
@@ -40,7 +40,7 @@ namespace TaxCalculator.WebApi
                         "http://localhost:44331",
                         "https://localhost:44331",
                         "https://relaxed-bose-eb5bc2.netlify.com",
-                        "https://pensiontoolsblazor.z6.web.core.windows.net/")
+                        "https://pensiontoolsblazor.z6.web.core.windows.net")
                         .AllowAnyHeader()
                         .AllowAnyMethod();
                 }));
