212ab5e7e92af7deb61dc814cdc979d03f8f7d7d
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2022-10-12 11:00:44 +0200

    SAVEPOINT

---
+++ src/Calculators.Cashflow.Abstractions/Accounts/SalaryAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculators.CashFlow.Accounts;
+
+public class SalaryAccount
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public decimal Balance { get; set; }
+
+    public IEnumerable<AccountTransaction> Transactions { get; set; }
+}
