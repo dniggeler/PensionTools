82bc8c2c1489d31e69303d856a547a8690e71f66
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2023-06-08 09:07:32 +0200

    migrate edit and listing person

--- src/BlazorApp/ViewModels/PersonViewModel.cs
+++ src/BlazorApp/ViewModels/PersonViewModel.cs
@@ -2,42 +2,56 @@
 using System.ComponentModel.DataAnnotations;
 using PensionCoach.Tools.CommonTypes;
 
-namespace BlazorApp.ViewModels
+namespace BlazorApp.ViewModels;
+
+public record PersonViewModel
 {
-    public record PersonViewModel
+    public Guid Id { get; set; } = Guid.NewGuid();
+        
+    [Required]
+    [StringLength(25, ErrorMessage = "Name ist zu lang.", ErrorMessageResourceName = "error.person.name")]
+    public string Name { get; set; }
+        
+    public DateTime? DateOfBirth { get; set; }
+
+    public Gender Gender { get; set; }
+        
+    public CivilStatus CivilStatus { get; set; }
+        
+    public int NumberOfChildren { get; set; }
+        
+    public ReligiousGroupType ReligiousGroupType { get; set; }
+        
+    public ReligiousGroupType? PartnerReligiousGroupType { get; set; }
+
+    public decimal TaxableIncome { get; set; }
+
+    public decimal TaxableFederalIncome => TaxableIncome;
+        
+    public decimal TaxableWealth { get; set; }
+
+    public int BfsMunicipalityId { get; set; }
+
+    public Canton Canton { get; set; }
+
+    public string MunicipalityName { get; set; }
+
+    public decimal FinalRetirementCapital { get; set; }
+
+    public decimal FinalCapital3a { get; set; }
+
+    public PersonViewModel Update(PersonViewModel source)
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        
-        [Required]
-        [StringLength(25, ErrorMessage = "Name ist zu lang.", ErrorMessageResourceName = "error.person.name")]
-        public string Name { get; set; }
-        
-        public DateTime DateOfBirth { get; set; }
+        Name = source.Name;
+        CivilStatus = source.CivilStatus;
+        DateOfBirth = source.DateOfBirth;
+        Gender = source.Gender;
+        TaxableIncome = source.TaxableIncome;
+        TaxableWealth = source.TaxableWealth;
+        Canton = source.Canton;
+        FinalCapital3a = source.FinalCapital3a;
+        FinalRetirementCapital = source.FinalRetirementCapital;
 
-        public Gender Gender { get; set; }
-        
-        public CivilStatus CivilStatus { get; set; }
-        
-        public int NumberOfChildren { get; set; }
-        
-        public ReligiousGroupType ReligiousGroupType { get; set; }
-        
-        public ReligiousGroupType? PartnerReligiousGroupType { get; set; }
-
-        public decimal TaxableIncome { get; set; }
-
-        public decimal TaxableFederalIncome => TaxableIncome;
-        
-        public decimal TaxableWealth { get; set; }
-
-        public int BfsMunicipalityId { get; set; }
-
-        public Canton Canton { get; set; }
-
-        public string MunicipalityName { get; set; }
-
-        public decimal FinalRetirementCapital { get; set; }
-
-        public decimal FinalCapital3a { get; set; }
+        return this;
     }
 }
