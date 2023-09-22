0497410d109e77069be33097504f3a5ac4e2cb14
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2023-09-22 15:04:34 +0200

    SAVEPOINT

--- src/Tax.Tools.Comparison.Domain/TaxComparerResultReportModel.cs
+++ src/Tax.Tools.Comparison.Domain/TaxComparerResultReportModel.cs
@@ -1,23 +1,22 @@
 ﻿using Domain.Enums;
 
-namespace PensionCoach.Tools.TaxComparison
+namespace PensionCoach.Tools.TaxComparison;
+
+public class TaxComparerResultReportModel
 {
-    public class TaxComparerResultReportModel
-    {
-        public int MunicipalityId { get; set; }
+    public int MunicipalityId { get; set; }
 
-        public string MunicipalityName { get; set; }
+    public string MunicipalityName { get; set; }
 
-        public Canton Canton { get; set; }
+    public Canton Canton { get; set; }
 
-        public decimal TotalTaxAmount { get; set; }
+    public decimal TotalTaxAmount { get; set; }
 
-        public decimal FederalTaxAmount { get; set; }
+    public decimal FederalTaxAmount { get; set; }
 
-        public decimal MunicipalityTaxAmount { get; set; }
+    public decimal MunicipalityTaxAmount { get; set; }
 
-        public decimal CantonTaxAmount { get; set; }
+    public decimal CantonTaxAmount { get; set; }
 
-        public decimal ChurchTaxAmount { get; set; }
-    }
+    public decimal ChurchTaxAmount { get; set; }
 }
