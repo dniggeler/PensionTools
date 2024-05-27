5fbdb34c262abc6a977bf281ded8694b65780f55
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2024-05-27 08:46:01 +0200

    SAVEPOINT

--- src/AspireHost/Program.cs
+++ src/AspireHost/Program.cs
@@ -5,7 +5,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var apiService = builder.AddProject<TaxCalculator_WebApi>("apiservice");
 builder
     .AddProject<BlazorApp>("webfrontend")
-    .WithReference(apiService)
-    .WithExternalHttpEndpoints();
+    .WithReference(apiService);
 
 builder.Build().Run();
