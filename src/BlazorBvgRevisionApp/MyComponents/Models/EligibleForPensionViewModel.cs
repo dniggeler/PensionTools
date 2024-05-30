446b2819e8c496ff817dff4c370ab9c560d6d02f
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2024-05-30 16:16:39 +0200

    SAVEPOINT

---
+++ src/BlazorBvgRevisionApp/MyComponents/Models/EligibleForPensionViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorBvgRevisionApp.MyComponents.Models;
+
+public class EligibleForPensionViewModel
+{
+    public bool IsEligibleAhv { get; set; }
+    public bool IsEligibleBvg { get; set; }
+
+    public bool IsEligible() => IsEligibleBvg && IsEligibleAhv;
+}
