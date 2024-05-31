4bdfdd4ed71560a57de7e32bc8728257a498a444
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2024-05-31 10:12:46 +0200

    SAVEPOINT

---
+++ src/BlazorBvgRevisionApp/MyComponents/Models/TableResult.cs
@@ -0,0 +1,4 @@
+﻿public record TableResult(string Benefit, decimal ValueCurrentBvg, decimal? ValueBvgRevision)
+{
+    public decimal? Difference => ValueBvgRevision.HasValue ? Math.Round(ValueBvgRevision.Value - ValueCurrentBvg) : null;
+}
