339233d2aa2113248d35b7871f7375b87af44cca
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2024-06-29 10:45:51 +0200

    use feature flag for full revision support

--- src/BlazorBvgRevisionApp/Program.cs
+++ src/BlazorBvgRevisionApp/Program.cs
@@ -3,6 +3,7 @@ using BlazorBvgRevisionApp;
 using BlazorBvgRevisionApp.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FeatureManagement;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -13,6 +14,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 builder.Services.AddLocalization();
 builder.Services.AddBvgCalculators();
+builder.Services.AddFeatureManagement();
 
 builder.Services.AddScoped<PensionPlanSimulator>();
 
