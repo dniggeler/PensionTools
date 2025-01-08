4ad87762527599d9077513335ea1f81ef08e0e0b
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2025-01-08 17:34:53 +0100

    SAVEPOINT

---
+++ src/CashFlowCalculationEngine.Server/Domain/CashFlows/SingleCashFlowModel.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enums;
+
+namespace CashFlowCalculationEngine.Server.Domain.CashFlows;
+
+public record CashFlowModel(
+    DateOnly DateOfProcess,
+    decimal Amount,
+    Guid SourceAccountId,
+    Guid TargetAccountId,
+    bool IsTaxable,
+    TaxType TaxType);
