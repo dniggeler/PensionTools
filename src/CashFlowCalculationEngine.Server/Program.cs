6d661ff108ef9e64e991fb3f3d65c66a0daf9650
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2025-01-23 10:10:08 +0100

    SAVEPOINT

--- src/CashFlowCalculationEngine.Server/Program.cs
+++ src/CashFlowCalculationEngine.Server/Program.cs
@@ -6,8 +6,39 @@ using CashFlowCalculationEngine.Server.Queries;
 using Infrastructure.Configuration;
 using Infrastructure.EstvTaxCalculator;
 using Infrastructure.Tax.Data;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddOpenTelemetry(logging =>
+{
+    logging.IncludeFormattedMessage = true;
+    logging.IncludeScopes = true;
+});
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(metrics =>
+    {
+        metrics.AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddRuntimeInstrumentation();
+    })
+    .WithTracing(tracing =>
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            // We want to view all traces in development
+            tracing.SetSampler(new AlwaysOnSampler());
+        }
+
+        tracing.AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation();
+    });
+
+builder.Services.AddHealthChecks()
+    .AddCheck("self", () => HealthCheckResult.Healthy(), ["live"]);
 
 builder.Services.AddTransient<MultiPeriodCalculationRequestValidator>();
 builder.Services.AddTransient<IMultiPeriodCashFlowCalculator, MultiPeriodCashFlowCalculator>();
@@ -16,7 +47,8 @@ builder.Services.AddTaxData(builder.Configuration);
 builder.Services.AddTaxCalculators(builder.Configuration.GetApplicationMode());
 builder.Services.AddEstvTaxCalculatorClient(builder.Configuration);
 
-builder.Services.AddGraphQLServer()
+builder.Services
+    .AddGraphQLServer()
     .AddQueryType<MultiPeriodCashFlowQueries>()
     .ModifyOptions(c => c.EnableOneOf = true)
     .AddFluentValidation();
