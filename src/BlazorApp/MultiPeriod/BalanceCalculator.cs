6584b35577c74562c0878a8ef7fda543bb36edb2
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2025-01-23 23:46:50 +0100

    SAVEPOINT

---
+++ src/BlazorApp/MultiPeriod/BalanceCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Calculator;
+
+namespace BlazorApp.MultiPeriod;
+
+public class BalanceCalculator
+{
+    public BalanceSheet Calculate(DateTime startDate, DateTime endDate, ICalculate_Calculate_Transactions calculateTransactions)
+    {
+        BalanceSheet balanceSheet = new BalanceSheet();
+
+        balanceSheet.TotalWealth =
+            calculateTransactions.IncomeAccounts.SelectMany(a => a.Transactions)
+                .Sum(t => t.Amount) +
+            calculateTransactions.WealthAccounts.SelectMany(a => a.Transactions)
+                .Sum(t => t.Amount);
+
+        return balanceSheet;
+    }
+}
---
+++ src/BlazorApp/MultiPeriod/BalanceSheet.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp.MultiPeriod;
+
+public class BalanceSheet
+{
+    public decimal? TotalWealth { get; set; }
+
+    public decimal? TotalThirdPillar { get; set; }
+
+    public decimal? TotalOccupationalPension { get; set; }
+}
